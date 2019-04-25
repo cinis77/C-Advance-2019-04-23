7d23a0949bb91d3c3cbc247db4a09a87a010d0ad
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-04-25 19:59:12 +0300

    Uzduotys iki pertraukos

---
+++ _20190425Pirmauzduotis/_20190425Pirmauzduotis/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190425Pirmauzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Vardas = "Dominykas";
+            string Pavarde = "Rentelis";
+            int Amzius = 26;
+            float Ugis = 1.8f;
+            double Svoris = 95;
+            string aukstojiMokykla = "Kauno Technologijos Universitetas";
+            string AkademinesGrupesKodas = "RBB15";
+            int Kursas = 2;
+            string StudijuProgramosPavadinimas = "Programavimas";
+            int AtsiskaitytuKredituSkaicius = 60;
+
+            Console.WriteLine("TO DO sarasas:");
+            Console.WriteLine("1. Isvesti suni");
+            Console.WriteLine("2. Nupirkti pieno");
+            Console.WriteLine("3. Isnesti siusksles");
+            Console.WriteLine("TO DO sarasas \n1.\tIsvesti suni\n2.\tNupirkti pieno\n3.\tIsnesti siuskles");
+            Console.WriteLine();
+            Console.WriteLine("STUDENTO DUOMENYS");
+            Console.WriteLine("{0} {1} ({2}m.)", Vardas, Pavarde, Amzius);
+            Console.WriteLine("Jo ugis: {0}m., svoris: {1:0.000}kg", Ugis, Svoris);
+            Console.WriteLine("Jis mokosi aukstojoje mokykloje: " + aukstojiMokykla + "({0})", StudijuProgramosPavadinimas);
+            Console.WriteLine("Akademine grupe kuriai priklauso yra: " + AkademinesGrupesKodas );
+            Console.WriteLine("Jau yra baiges {0}, siuo metu mokosi {1} kurse", (Kursas - 1), Kursas);
+            Console.WriteLine("Per studijas yra atsiskaites uz {0} kreditu", AtsiskaitytuKredituSkaicius);
+
+            Console.WriteLine("Iveskite raide");
+            char raide = Convert.ToChar(Console.ReadKey().KeyChar);
+            Console.WriteLine("\n ivedete raide: " + raide);
+
+            Console.WriteLine("Iveskite sakini: ");
+            var sakinys = Console.ReadLine();
+            Console.WriteLine("Jusu ivestas sakinys: \n" + sakinys);
+        }
+    }
+}
