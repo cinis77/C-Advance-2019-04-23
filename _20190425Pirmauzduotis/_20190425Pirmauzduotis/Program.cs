ece2cf796df251bc1b09a8ee1f35343887b2d8c9
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-04-25 20:23:07 +0300

    Uzduotis paskutine su ivedimu duomenu is konsole

--- _20190425Pirmauzduotis/_20190425Pirmauzduotis/Program.cs
+++ _20190425Pirmauzduotis/_20190425Pirmauzduotis/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace _20190425Pirmauzduotis
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string Vardas = "Dominykas";
             string Pavarde = "Rentelis";
@@ -31,17 +31,27 @@ namespace _20190425Pirmauzduotis
             Console.WriteLine("{0} {1} ({2}m.)", Vardas, Pavarde, Amzius);
             Console.WriteLine("Jo ugis: {0}m., svoris: {1:0.000}kg", Ugis, Svoris);
             Console.WriteLine("Jis mokosi aukstojoje mokykloje: " + aukstojiMokykla + "({0})", StudijuProgramosPavadinimas);
-            Console.WriteLine("Akademine grupe kuriai priklauso yra: " + AkademinesGrupesKodas );
+            Console.WriteLine("Akademine grupe kuriai priklauso yra: " + AkademinesGrupesKodas);
             Console.WriteLine("Jau yra baiges {0}, siuo metu mokosi {1} kurse", (Kursas - 1), Kursas);
             Console.WriteLine("Per studijas yra atsiskaites uz {0} kreditu", AtsiskaitytuKredituSkaicius);
 
             Console.WriteLine("Iveskite raide");
             char raide = Convert.ToChar(Console.ReadKey().KeyChar);
             Console.WriteLine("\n ivedete raide: " + raide);
-
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Iveskite sakini: ");
             var sakinys = Console.ReadLine();
-            Console.WriteLine("Jusu ivestas sakinys: \n" + sakinys);
+            Console.WriteLine("Jūsų ivestas sakinys: \n" + sakinys);
+
+            Console.WriteLine("Iveksite varda");
+            string var = Console.ReadLine();
+            Console.WriteLine("Iveskite megiamiausios spalvos pirma raide");
+            char r = Convert.ToChar(Console.ReadKey().KeyChar);
+            Console.WriteLine("IVeskite atsitiktini skaiciu");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("IVeskite atsitiktini skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(var + " " + r + " " + sk + " " + sk1);
         }
     }
 }
