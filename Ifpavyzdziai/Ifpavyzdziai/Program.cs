377d75776382f22e05d79fcba38846c81651f378
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-04-30 21:21:10 +0300

    uzduotys

---
+++ Ifpavyzdziai/Ifpavyzdziai/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ifpavyzdziai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int pirmas = 20;
+            int antras = 10;
+            int trecias = 5;
+
+            string something = null;
+            string a = something != null ? "A" : "B";
+
+            if (pirmas > antras && pirmas < 100)
+            {
+                Console.WriteLine("Pirma salyga yra tenkinama");
+            }
+            else if (antras > 0 && antras > pirmas)
+            {
+                Console.WriteLine("Antra salyga yra tenkinama");
+            }
+            else if (pirmas > antras && pirmas > trecias || pirmas > 0)
+            {
+                Console.WriteLine("Tenkinama trecia salyga");
+            }
+            else if (trecias >= 5 && trecias <= 10 || trecias > pirmas || trecias > antras)
+            {
+                Console.WriteLine("Ketvirta salyga yra tenkinama");
+            }
+        }
+    }
+}
---
+++ SwitchPirmasVariantas/SwitchPirmasVariantas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchPirmasVariantas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            switch (sk)
+            {
+                case 1:
+                    Console.WriteLine(sk + sk1 + sk2);
+                    break;
+
+                case 2:
+                    Console.WriteLine(sk - sk2);
+                    break;
+
+                case 3:
+                    Console.WriteLine(sk1 * sk2);
+                    break;
+
+                default:
+                    Console.WriteLine("Ivesta nesamone");
+                    break;
+            }
+        }
+    }
+}
