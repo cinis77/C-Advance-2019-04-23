2ab8a7ecc35c2107ec173eebbb4bc6f859d9a6ed
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-04-30 18:58:22 +0300

    Trikampiu uzduotis

---
+++ Trikampiai/Trikampiai/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trikampiai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            if (sk + sk1 < sk2)
+            {
+            }
+            else if (sk1 + sk2 < sk)
+            {
+            }
+            else if (sk + sk2 < sk1)
+            {
+            }
+            else
+            {
+            }
+            if (sk + sk1 > sk2 && sk1 + sk2 > sk && sk + sk2 > sk1)
+            {
+                Console.WriteLine("Trikampi sudaryti imanoma");
+            }
+            else
+            {
+                Console.WriteLine("Trikampio sudaryti neimanoma");
+            }
+        }
+    }
+}
