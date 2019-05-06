5cb2f3a2abeb3988213e7fc5c9962a63ed860867
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-05-06 21:09:15 +0300

    Romeniski

---
+++ IntMetodai/IntMetodai/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntMetodai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaicius");
+            var sk = Convert.ToInt32(Console.ReadLine());
+            var sk1 = Convert.ToInt32(Console.ReadLine());
+
+            if (int.TryParse(Console.ReadLine(), out sk))
+            {
+            }
+
+            Console.WriteLine(Suma(sk, sk1));
+            Console.WriteLine(Skirtumas(sk, sk1));
+            Console.WriteLine(Sandauga(sk, sk1));
+            Console.WriteLine(Dalyba(sk, sk1));
+        }
+
+        public static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Skirtumas(int a, int b)
+        {
+            return (a - b);
+        }
+
+        public static int Sandauga(int a, int b)
+        {
+            return (a * b);
+        }
+
+        public static double Dalyba(int a, int b)
+        {
+            return (double)a / b;
+        }
+    }
+}
---
+++ RomeniskiSkaiciai/RomeniskiSkaiciai/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomeniskiSkaiciai
+{
+    internal class Program
+    {
+        public static string Konvertavimas(int sk, string vienetas, string penketas, string desimt)
+        {
+            if (sk == 9)
+            {
+                return vienetas + desimt;
+            }
+            else if (sk == 4)
+            {
+                return vienetas + penketas;
+            }
+            else
+            {
+                int penki = sk / 5;
+                int vienetai = sk % 5;
+                string atsakymas = string.Empty;
+                if (penki != 0)
+                {
+                    atsakymas += penketas;
+                }
+                for (int i = 0; i < vienetai; i++)
+                {
+                    atsakymas += vienetas;
+                }
+                return atsakymas;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            if (skaicius < 1000)
+            {
+                int simtai = skaicius / 100;
+                int desimt = (skaicius / 10) % 10;
+                int vnt = skaicius % 10;
+                string romeniskai = Konvertavimas(simtai, "C", "D", "M")
+                    + Konvertavimas(desimt, "X", "L", "C")
+                    + Konvertavimas(vnt, "I", "V", "X");
+                Console.WriteLine(romeniskai);
+            }
+            else
+            {
+                Console.WriteLine("M");
+            }
+        }
+    }
+}
