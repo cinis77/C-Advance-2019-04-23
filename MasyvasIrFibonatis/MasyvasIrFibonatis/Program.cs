ba0a322acc937b9f41940b75dbbc4ce78b430f4c
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-05-09 21:28:45 +0300

    Masyvu paskutine

---
+++ MasyvasIrFibonatis/MasyvasIrFibonatis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasyvasIrFibonatis
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] seka = new int[10];
+            seka[0] = 1;
+            seka[1] = 1;
+            for (int i = 2; i < seka.Length; i++)
+            {
+                seka[i] = seka[i - 2] + seka[i - 1];
+            }
+
+            foreach (var item in seka)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Temperaturos/Temperaturos/Program.cs
+++ Temperaturos/Temperaturos/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace Temperaturos
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Random rng = new Random();
+            int[] temperaturos = new int[rng.Next(2000)];
+            for (int i = 0; i < temperaturos.Length; i++)
+            {
+                temperaturos[i] = rng.Next(-30, 30);
+            }
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int sum = 0;
+            foreach (var item in temperaturos)
+            {
+                if (min > item)
+                {
+                    min = item;
+                }
+                if (max < item)
+                {
+                    max = item;
+                }
+                sum += item;
+            }
+            double vidurkis = (double)sum / temperaturos.Length;
+            int sumaMaziauVidurkio = 0;
+            foreach (var item in temperaturos)
+            {
+                if (item < vidurkis)
+                {
+                    sumaMaziauVidurkio += item;
+                }
+            }
+            Console.WriteLine("Maziausia temperatura: " + min);
+            Console.WriteLine("Didziausia temperatura: " + max);
+            Console.WriteLine("Vidurkis: " + ((double)sum / temperaturos.Length));
+            Console.WriteLine("Mazesniu uz vidurkis suma: " + sumaMaziauVidurkio);
         }
     }
 }
