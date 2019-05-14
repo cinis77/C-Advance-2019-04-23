770a44a438621b8a6511269d522595ff3044f931
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-05-14 20:14:29 +0300

    Klases su pvz

---
+++ Klases/Klases/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klases
+{
+    internal class Automobilis
+    {
+        private int _Rida;
+        public readonly string Markė;
+        public string Gamintojas;
+        private int GamybosMetai; // _GamybosMetai
+
+        public int Rida
+        {
+            get
+            {
+                return _Rida;
+            }
+            set
+            {
+                if (value != _Rida)
+                {
+                    _Rida = value;
+                    NuvaziuotaPerMetus();
+                }
+            }
+        }
+
+        public string PropertyTest { get; set; }
+
+        public double NuvaziuotaPerMetus()
+        {
+            Rida = 1000;
+            return (double)Rida / (DateTime.Today.Year - GamybosMetai);
+        }
+
+        public double NuvaziuotaPerMetus(double rida)
+        {
+            return rida / (DateTime.Today.Year - GamybosMetai);
+        }
+
+        public void GamybosMetaiSet(int GamybosMetai)
+        {
+            this.GamybosMetai = GamybosMetai;
+        }
+
+        public int GamybosMetaiGet()
+        {
+            return GamybosMetai;
+        }
+
+        public Automobilis(string marke)
+        {
+            Markė = marke;
+        }
+
+        public Automobilis()
+        {
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Automobilis auto = new Automobilis("A6");
+            Automobilis auto1 = new Automobilis("SLS");
+            Automobilis auto2 = new Automobilis();
+            auto.Rida = 200000;
+
+            Console.WriteLine(auto.NuvaziuotaPerMetus());
+        }
+    }
+}
---
+++ KlasesPavyzdys/KlasesPavyzdys/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasesPavyzdys
+{
+    internal class Namas
+    {
+        public int Aukštai;
+        public string Adresas;
+        public string Tipas { get; set; }
+        public string Medžiagiškumas { get; set; }
+
+        #region Construtor field
+
+        public Namas()
+        {
+        }
+
+        public Namas(int aukstai, string adresas, string tipas, string medziagiskumas)
+        {
+            Aukštai = aukstai;
+            Adresas = adresas;
+            Tipas = tipas;
+            Medžiagiškumas = medziagiskumas;
+        }
+
+        #endregion Construtor field
+
+        public int KiekMetuJauStovi(int statybosMetai)
+        {
+            return DateTime.Today.Year - statybosMetai;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Namas pirmas = new Namas(); // Tuscias kontruktorius
+            Namas antras = new Namas(2, "Kovo 10", "Blokinis", "Geras"); // Sukurtas netuscias konstruktorius
+            Namas trecias = new Namas()
+            {
+                Adresas = "Kovo 10",
+                Tipas = "Murinis",
+                Medžiagiškumas = "Baisus",
+                Aukštai = 1
+            };
+        }
+    }
+}
