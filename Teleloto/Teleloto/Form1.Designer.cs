422524c6e8ace4ba460c8350260237cf93bf5b3f
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-05-16 21:16:59 +0300

    Teleloto zaidimas

---
+++ Teleloto/Teleloto/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace Teleloto
+{
+    partial class Teleloto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BilietasGroup = new System.Windows.Forms.GroupBox();
+            this.Generuoti = new System.Windows.Forms.Button();
+            this.Z5 = new System.Windows.Forms.TextBox();
+            this.Z4 = new System.Windows.Forms.TextBox();
+            this.Z3 = new System.Windows.Forms.TextBox();
+            this.Z2 = new System.Windows.Forms.TextBox();
+            this.Z1 = new System.Windows.Forms.TextBox();
+            this.G5 = new System.Windows.Forms.TextBox();
+            this.G4 = new System.Windows.Forms.TextBox();
+            this.G3 = new System.Windows.Forms.TextBox();
+            this.G2 = new System.Windows.Forms.TextBox();
+            this.G1 = new System.Windows.Forms.TextBox();
+            this.R5 = new System.Windows.Forms.TextBox();
+            this.R4 = new System.Windows.Forms.TextBox();
+            this.R3 = new System.Windows.Forms.TextBox();
+            this.R2 = new System.Windows.Forms.TextBox();
+            this.R1 = new System.Windows.Forms.TextBox();
+            this.J5 = new System.Windows.Forms.TextBox();
+            this.J4 = new System.Windows.Forms.TextBox();
+            this.J3 = new System.Windows.Forms.TextBox();
+            this.J2 = new System.Windows.Forms.TextBox();
+            this.J1 = new System.Windows.Forms.TextBox();
+            this.M5 = new System.Windows.Forms.TextBox();
+            this.M4 = new System.Windows.Forms.TextBox();
+            this.M3 = new System.Windows.Forms.TextBox();
+            this.M2 = new System.Windows.Forms.TextBox();
+            this.M1 = new System.Windows.Forms.TextBox();
+            this.Zaidimas = new System.Windows.Forms.GroupBox();
+            this.Isridenti = new System.Windows.Forms.TextBox();
+            this.Zaisti = new System.Windows.Forms.Button();
+            this.BilietasGroup.SuspendLayout();
+            this.Zaidimas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BilietasGroup
+            // 
+            this.BilietasGroup.Controls.Add(this.Generuoti);
+            this.BilietasGroup.Controls.Add(this.Z5);
+            this.BilietasGroup.Controls.Add(this.Z4);
+            this.BilietasGroup.Controls.Add(this.Z3);
+            this.BilietasGroup.Controls.Add(this.Z2);
+            this.BilietasGroup.Controls.Add(this.Z1);
+            this.BilietasGroup.Controls.Add(this.G5);
+            this.BilietasGroup.Controls.Add(this.G4);
+            this.BilietasGroup.Controls.Add(this.G3);
+            this.BilietasGroup.Controls.Add(this.G2);
+            this.BilietasGroup.Controls.Add(this.G1);
+            this.BilietasGroup.Controls.Add(this.R5);
+            this.BilietasGroup.Controls.Add(this.R4);
+            this.BilietasGroup.Controls.Add(this.R3);
+            this.BilietasGroup.Controls.Add(this.R2);
+            this.BilietasGroup.Controls.Add(this.R1);
+            this.BilietasGroup.Controls.Add(this.J5);
+            this.BilietasGroup.Controls.Add(this.J4);
+            this.BilietasGroup.Controls.Add(this.J3);
+            this.BilietasGroup.Controls.Add(this.J2);
+            this.BilietasGroup.Controls.Add(this.J1);
+            this.BilietasGroup.Controls.Add(this.M5);
+            this.BilietasGroup.Controls.Add(this.M4);
+            this.BilietasGroup.Controls.Add(this.M3);
+            this.BilietasGroup.Controls.Add(this.M2);
+            this.BilietasGroup.Controls.Add(this.M1);
+            this.BilietasGroup.Location = new System.Drawing.Point(7, 7);
+            this.BilietasGroup.Margin = new System.Windows.Forms.Padding(2);
+            this.BilietasGroup.Name = "BilietasGroup";
+            this.BilietasGroup.Padding = new System.Windows.Forms.Padding(2);
+            this.BilietasGroup.Size = new System.Drawing.Size(378, 271);
+            this.BilietasGroup.TabIndex = 0;
+            this.BilietasGroup.TabStop = false;
+            this.BilietasGroup.Text = "Bilietas";
+            // 
+            // Generuoti
+            // 
+            this.Generuoti.Location = new System.Drawing.Point(245, 36);
+            this.Generuoti.Name = "Generuoti";
+            this.Generuoti.Size = new System.Drawing.Size(75, 23);
+            this.Generuoti.TabIndex = 25;
+            this.Generuoti.Text = "Generuoti";
+            this.Generuoti.UseVisualStyleBackColor = true;
+            this.Generuoti.Click += new System.EventHandler(this.Generuo_Click);
+            // 
+            // Z5
+            // 
+            this.Z5.Location = new System.Drawing.Point(195, 114);
+            this.Z5.Margin = new System.Windows.Forms.Padding(2);
+            this.Z5.Name = "Z5";
+            this.Z5.Size = new System.Drawing.Size(30, 20);
+            this.Z5.TabIndex = 24;
+            // 
+            // Z4
+            // 
+            this.Z4.Location = new System.Drawing.Point(195, 95);
+            this.Z4.Margin = new System.Windows.Forms.Padding(2);
+            this.Z4.Name = "Z4";
+            this.Z4.Size = new System.Drawing.Size(30, 20);
+            this.Z4.TabIndex = 23;
+            // 
+            // Z3
+            // 
+            this.Z3.Location = new System.Drawing.Point(195, 75);
+            this.Z3.Margin = new System.Windows.Forms.Padding(2);
+            this.Z3.Name = "Z3";
+            this.Z3.Size = new System.Drawing.Size(30, 20);
+            this.Z3.TabIndex = 22;
+            // 
+            // Z2
+            // 
+            this.Z2.Location = new System.Drawing.Point(195, 56);
+            this.Z2.Margin = new System.Windows.Forms.Padding(2);
+            this.Z2.Name = "Z2";
+            this.Z2.Size = new System.Drawing.Size(30, 20);
+            this.Z2.TabIndex = 21;
+            // 
+            // Z1
+            // 
+            this.Z1.Location = new System.Drawing.Point(195, 36);
+            this.Z1.Margin = new System.Windows.Forms.Padding(2);
+            this.Z1.Name = "Z1";
+            this.Z1.Size = new System.Drawing.Size(30, 20);
+            this.Z1.TabIndex = 20;
+            // 
+            // G5
+            // 
+            this.G5.Location = new System.Drawing.Point(149, 114);
+            this.G5.Margin = new System.Windows.Forms.Padding(2);
+            this.G5.Name = "G5";
+            this.G5.Size = new System.Drawing.Size(30, 20);
+            this.G5.TabIndex = 19;
+            // 
+            // G4
+            // 
+            this.G4.Location = new System.Drawing.Point(149, 95);
+            this.G4.Margin = new System.Windows.Forms.Padding(2);
+            this.G4.Name = "G4";
+            this.G4.Size = new System.Drawing.Size(30, 20);
+            this.G4.TabIndex = 18;
+            // 
+            // G3
+            // 
+            this.G3.Location = new System.Drawing.Point(149, 75);
+            this.G3.Margin = new System.Windows.Forms.Padding(2);
+            this.G3.Name = "G3";
+            this.G3.Size = new System.Drawing.Size(30, 20);
+            this.G3.TabIndex = 17;
+            // 
+            // G2
+            // 
+            this.G2.Location = new System.Drawing.Point(149, 56);
+            this.G2.Margin = new System.Windows.Forms.Padding(2);
+            this.G2.Name = "G2";
+            this.G2.Size = new System.Drawing.Size(30, 20);
+            this.G2.TabIndex = 16;
+            // 
+            // G1
+            // 
+            this.G1.Location = new System.Drawing.Point(149, 36);
+            this.G1.Margin = new System.Windows.Forms.Padding(2);
+            this.G1.Name = "G1";
+            this.G1.Size = new System.Drawing.Size(30, 20);
+            this.G1.TabIndex = 15;
+            // 
+            // R5
+            // 
+            this.R5.Location = new System.Drawing.Point(104, 114);
+            this.R5.Margin = new System.Windows.Forms.Padding(2);
+            this.R5.Name = "R5";
+            this.R5.Size = new System.Drawing.Size(30, 20);
+            this.R5.TabIndex = 14;
+            // 
+            // R4
+            // 
+            this.R4.Location = new System.Drawing.Point(104, 95);
+            this.R4.Margin = new System.Windows.Forms.Padding(2);
+            this.R4.Name = "R4";
+            this.R4.Size = new System.Drawing.Size(30, 20);
+            this.R4.TabIndex = 13;
+            // 
+            // R3
+            // 
+            this.R3.Location = new System.Drawing.Point(104, 75);
+            this.R3.Margin = new System.Windows.Forms.Padding(2);
+            this.R3.Name = "R3";
+            this.R3.Size = new System.Drawing.Size(30, 20);
+            this.R3.TabIndex = 12;
+            // 
+            // R2
+            // 
+            this.R2.Location = new System.Drawing.Point(104, 56);
+            this.R2.Margin = new System.Windows.Forms.Padding(2);
+            this.R2.Name = "R2";
+            this.R2.Size = new System.Drawing.Size(30, 20);
+            this.R2.TabIndex = 11;
+            // 
+            // R1
+            // 
+            this.R1.Location = new System.Drawing.Point(104, 36);
+            this.R1.Margin = new System.Windows.Forms.Padding(2);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(30, 20);
+            this.R1.TabIndex = 10;
+            // 
+            // J5
+            // 
+            this.J5.Location = new System.Drawing.Point(60, 114);
+            this.J5.Margin = new System.Windows.Forms.Padding(2);
+            this.J5.Name = "J5";
+            this.J5.Size = new System.Drawing.Size(30, 20);
+            this.J5.TabIndex = 9;
+            // 
+            // J4
+            // 
+            this.J4.Location = new System.Drawing.Point(60, 95);
+            this.J4.Margin = new System.Windows.Forms.Padding(2);
+            this.J4.Name = "J4";
+            this.J4.Size = new System.Drawing.Size(30, 20);
+            this.J4.TabIndex = 8;
+            // 
+            // J3
+            // 
+            this.J3.Location = new System.Drawing.Point(60, 75);
+            this.J3.Margin = new System.Windows.Forms.Padding(2);
+            this.J3.Name = "J3";
+            this.J3.Size = new System.Drawing.Size(30, 20);
+            this.J3.TabIndex = 7;
+            // 
+            // J2
+            // 
+            this.J2.Location = new System.Drawing.Point(60, 56);
+            this.J2.Margin = new System.Windows.Forms.Padding(2);
+            this.J2.Name = "J2";
+            this.J2.Size = new System.Drawing.Size(30, 20);
+            this.J2.TabIndex = 6;
+            // 
+            // J1
+            // 
+            this.J1.Location = new System.Drawing.Point(60, 36);
+            this.J1.Margin = new System.Windows.Forms.Padding(2);
+            this.J1.Name = "J1";
+            this.J1.Size = new System.Drawing.Size(30, 20);
+            this.J1.TabIndex = 5;
+            // 
+            // M5
+            // 
+            this.M5.Location = new System.Drawing.Point(20, 114);
+            this.M5.Margin = new System.Windows.Forms.Padding(2);
+            this.M5.Name = "M5";
+            this.M5.Size = new System.Drawing.Size(30, 20);
+            this.M5.TabIndex = 4;
+            // 
+            // M4
+            // 
+            this.M4.Location = new System.Drawing.Point(20, 95);
+            this.M4.Margin = new System.Windows.Forms.Padding(2);
+            this.M4.Name = "M4";
+            this.M4.Size = new System.Drawing.Size(30, 20);
+            this.M4.TabIndex = 3;
+            // 
+            // M3
+            // 
+            this.M3.Location = new System.Drawing.Point(20, 75);
+            this.M3.Margin = new System.Windows.Forms.Padding(2);
+            this.M3.Name = "M3";
+            this.M3.Size = new System.Drawing.Size(30, 20);
+            this.M3.TabIndex = 2;
+            // 
+            // M2
+            // 
+            this.M2.Location = new System.Drawing.Point(20, 56);
+            this.M2.Margin = new System.Windows.Forms.Padding(2);
+            this.M2.Name = "M2";
+            this.M2.Size = new System.Drawing.Size(30, 20);
+            this.M2.TabIndex = 1;
+            // 
+            // M1
+            // 
+            this.M1.Location = new System.Drawing.Point(20, 36);
+            this.M1.Margin = new System.Windows.Forms.Padding(2);
+            this.M1.Name = "M1";
+            this.M1.Size = new System.Drawing.Size(30, 20);
+            this.M1.TabIndex = 0;
+            // 
+            // Zaidimas
+            // 
+            this.Zaidimas.Controls.Add(this.Zaisti);
+            this.Zaidimas.Controls.Add(this.Isridenti);
+            this.Zaidimas.Location = new System.Drawing.Point(7, 295);
+            this.Zaidimas.Name = "Zaidimas";
+            this.Zaidimas.Size = new System.Drawing.Size(378, 281);
+            this.Zaidimas.TabIndex = 1;
+            this.Zaidimas.TabStop = false;
+            this.Zaidimas.Text = "Zaidimias";
+            // 
+            // Isridenti
+            // 
+            this.Isridenti.Location = new System.Drawing.Point(6, 19);
+            this.Isridenti.Multiline = true;
+            this.Isridenti.Name = "Isridenti";
+            this.Isridenti.Size = new System.Drawing.Size(248, 232);
+            this.Isridenti.TabIndex = 0;
+            // 
+            // Zaisti
+            // 
+            this.Zaisti.Location = new System.Drawing.Point(261, 20);
+            this.Zaisti.Name = "Zaisti";
+            this.Zaisti.Size = new System.Drawing.Size(75, 23);
+            this.Zaisti.TabIndex = 1;
+            this.Zaisti.Text = "Zaisti";
+            this.Zaisti.UseVisualStyleBackColor = true;
+            this.Zaisti.Click += new System.EventHandler(this.Zaisti_Click);
+            // 
+            // Teleloto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 600);
+            this.Controls.Add(this.Zaidimas);
+            this.Controls.Add(this.BilietasGroup);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Teleloto";
+            this.Text = "Teleloto";
+            this.BilietasGroup.ResumeLayout(false);
+            this.BilietasGroup.PerformLayout();
+            this.Zaidimas.ResumeLayout(false);
+            this.Zaidimas.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox BilietasGroup;
+        private System.Windows.Forms.TextBox Z5;
+        private System.Windows.Forms.TextBox Z4;
+        private System.Windows.Forms.TextBox Z3;
+        private System.Windows.Forms.TextBox Z2;
+        private System.Windows.Forms.TextBox Z1;
+        private System.Windows.Forms.TextBox G5;
+        private System.Windows.Forms.TextBox G4;
+        private System.Windows.Forms.TextBox G3;
+        private System.Windows.Forms.TextBox G2;
+        private System.Windows.Forms.TextBox G1;
+        private System.Windows.Forms.TextBox R5;
+        private System.Windows.Forms.TextBox R4;
+        private System.Windows.Forms.TextBox R3;
+        private System.Windows.Forms.TextBox R2;
+        private System.Windows.Forms.TextBox R1;
+        private System.Windows.Forms.TextBox J5;
+        private System.Windows.Forms.TextBox J4;
+        private System.Windows.Forms.TextBox J3;
+        private System.Windows.Forms.TextBox J2;
+        private System.Windows.Forms.TextBox J1;
+        private System.Windows.Forms.TextBox M5;
+        private System.Windows.Forms.TextBox M4;
+        private System.Windows.Forms.TextBox M3;
+        private System.Windows.Forms.TextBox M2;
+        private System.Windows.Forms.TextBox M1;
+        private System.Windows.Forms.Button Generuoti;
+        private System.Windows.Forms.GroupBox Zaidimas;
+        private System.Windows.Forms.TextBox Isridenti;
+        private System.Windows.Forms.Button Zaisti;
+    }
+}
+
---
+++ Teleloto/Teleloto/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teleloto
+{
+    public partial class Teleloto : Form
+    {
+        public Teleloto()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Mygtuko paspaudimo ivykis
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Generuo_Click(object sender, EventArgs e)
+        {
+            TextBox[] melyniLangeliai = { M1, M2, M3, M4, M5 };
+            TextBox[] juodiLangeliai = { J1, J2, J3, J4, J5 };
+            TextBox[] raudoniLangeliai = { R1, R2, R3, R4, R5 };
+            TextBox[] geltoniLangeliai = { G1, G2, G3, G4, G5 };
+            TextBox[] zaliLAngeliai = { Z1, Z2, Z3, Z4, Z5 };
+            GeneruotiKamuoliukus(melyniLangeliai, 1, 16);
+            GeneruotiKamuoliukus(juodiLangeliai, 16, 31);
+            GeneruotiKamuoliukus(raudoniLangeliai, 31, 46);
+            GeneruotiKamuoliukus(geltoniLangeliai, 46, 61);
+            GeneruotiKamuoliukus(zaliLAngeliai, 61, 76);
+        }
+
+        private void GeneruotiKamuoliukus(TextBox[] laukeliai, int a, int b)
+        {
+            Random rng = new Random();
+            int i = 0;
+            while (i < laukeliai.Length)
+            {
+                int kamuoliukas = rng.Next(a, b);
+                bool nevienodas = true;
+                foreach (var item in laukeliai)
+                {
+                    if (string.IsNullOrEmpty(item.Text))
+                    {
+                        continue;
+                    }
+                    if (kamuoliukas == int.Parse(item.Text))
+                    {
+                        nevienodas = false;
+                        break;
+                    }
+                }
+                if (nevienodas)
+                {
+                    laukeliai[i].Text = kamuoliukas.ToString();
+                    i++;
+                }
+            }
+        }
+
+        private void Zaisti_Click(object sender, EventArgs e)
+        {
+            List<int> istrauktiKamuoliukai = new List<int>();
+            int i = 0;
+            Random rng = new Random();
+            while (i < 47)
+            {
+                int kamuoliukas = rng.Next(1, 76);
+                bool nevienodas = true;
+                foreach (var item in istrauktiKamuoliukai)
+                {
+                    if (item == kamuoliukas)
+                    {
+                        nevienodas = false;
+                        break;
+                    }
+                }
+                if (nevienodas)
+                {
+                    istrauktiKamuoliukai.Add(kamuoliukas);
+                    Isridenti.Text += kamuoliukas + " ";
+                    i++;
+                    TikrintiBilieta(kamuoliukas);
+                }
+            }
+        }
+
+        private void TikrintiBilieta(int kamuoliukas)
+        {
+            TextBox[] visiKamuoliukai =
+                {
+                M1, M2, M3, M4, M5 ,
+                J1, J2, J3, J4, J5,
+                R1, R2, R3, R4, R5,
+                G1, G2, G3, G4, G5,
+                Z1, Z2, Z3, Z4, Z5
+            };
+            foreach (var item in visiKamuoliukai)
+            {
+                if (item.Text == kamuoliukas.ToString())
+                {
+                    item.BackColor = Color.Green;
+                }
+            }
+        }
+    }
+}
