988e10fc48e504f3b13f116bcba1eabaeead9084
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-05-21 18:30:07 +0300

    Klientu pavyzdys

---
+++ Klientai/Klientai/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klientai
+{
+    internal class Orders
+    {
+        public int ID { get; set; }
+        public string Pavadinimas { get; set; }
+        public double Kaina { get; set; }
+    }
+}
---
+++ Klientai/Klientai/Shipment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klientai
+{
+    internal class Shipment
+    {
+        public int Kaina { get; set; }
+        public string Tipas { get; set; }
+    }
+}
