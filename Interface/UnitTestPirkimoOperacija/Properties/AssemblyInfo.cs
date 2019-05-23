d20883217dff2cdf21e4776d8b7d7b080042c026
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-05-23 19:39:58 +0300

    Interface ir unitest

---
+++ Interface/UnitTestPirkimoOperacija/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPirkimoOperacija")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPirkimoOperacija")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1015eed8-4664-490a-bfc5-97581bf99165")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Interface/UnitTestPirkimoOperacija/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Interfacas;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestPirkimoOperacija
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IdetiIKrepseli_AssertstringEquealsstring()
+        {
+            FakeShipment fake = new FakeShipment(); //Mock
+            PirkimoOperacija pirkimas = new PirkimoOperacija(fake); // Testuojamas objektas
+
+            string mock = "mock"; //Informacinis mock
+            pirkimas.ĮdėtiĮkrepšelį(mock); //Prisidedam informacija
+            List<string> mockString = new List<string>() { mock }; // Sukuriam toki pat elementa
+            Assert.AreEqual(mockString.First(), pirkimas.GautiPirkiniuKrepseli().First()); // Patikrinam ar informacija sutampa
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SkaiciuotiSuma_ExceptionExpected()
+        {
+            FakeShipment fake = new FakeShipment();
+            PirkimoOperacija pirkimas = new PirkimoOperacija(fake);
+            pirkimas.SkaiciuotiVisaSuma();
+        }
+
+        [TestMethod]
+        public void SkaiciuotiVisaSuma_AddMockElement_Assert2()
+        {
+            FakeShipment fake = new FakeShipment(); //Mock
+            PirkimoOperacija pirkimas = new PirkimoOperacija(fake); // Testuojamas objektas
+
+            string mock = "mock"; //Informacinis mock
+            pirkimas.ĮdėtiĮkrepšelį(mock); //Prisidedam informacija
+
+            Assert.AreEqual(2, pirkimas.SkaiciuotiVisaSuma());
+        }
+    }
+
+    internal class FakeShipment : IshipementSkaiciuokle
+    {
+        public double svorisSiuntos { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public double SiuntosDydis(List<string> prekes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SiuntosSuma()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SiuntosSuma(List<string> prekes)
+        {
+            return 1;
+        }
+
+        public double SiuntosSvoris(string preke, int svoris)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
